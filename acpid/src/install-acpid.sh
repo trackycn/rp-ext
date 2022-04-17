9aa565c5fa0b85a0edd423099db7498d8f35be27
dodo-dk <dk-dodo@web.de>
2022-04-18 01:13:44 +0200

    Test

--- acpid/src/install-acpid.sh
+++ acpid/src/install-acpid.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 # copy file
-tar -zxvf /tmp/acpid.tar.gz -C /
-#tar -zxvf acpid.tar.gz -C /tmpRoot/
+#tar -zxvf /tmp/acpid.tar.gz -C /
+tar -zxvf acpid.tar.gz -C /tmpRoot/
 
 # enable
 systemctl enable acpid.service
